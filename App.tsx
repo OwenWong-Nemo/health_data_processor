5409cb39607e89e4e6b275f5ae9639e1a8f5857b
Chow Chung Yan <136058950+ccyccyccyccyccyccy@users.noreply.github.com>
2025-01-09 13:23:41 +0800

    basic UI for customise order

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,7 +15,14 @@ import {
   Text,
   useColorScheme,
   View,
+  PanResponder,
+  Dimensions, Button, Alert
 } from 'react-native';
+//import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RadioButton } from 'react-native-paper';
+//import  {DropdownComponent, CheckboxComponent}  from './Dropdown';
+import DropdownComponent from './Dropdown';
+//import AntDesign from '@expo/vector-icons/AntDesign'; //dependency issue
 
 import {
   Colors,
@@ -24,6 +31,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { CheckBox } from '@rneui/base';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -62,6 +70,29 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  //slidable : brew temp
+  const [barPosition, setBarPosition] = useState<number>(0);
+  const screenWidth = Dimensions.get('window').width;
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gestureState) => {
+      let newPosition = gestureState.moveX;
+
+      // Ensure the bar stays within screen bounds
+      if (newPosition < 0) newPosition = 0;
+      if (newPosition > screenWidth-50) newPosition = screenWidth-50; //don't know why 50
+
+      setBarPosition(newPosition);
+    },
+  });
+  let percentage = Math.round((barPosition / (screenWidth-50)) * 100);
+
+  //radio button: grind size 
+  const [grindValue, setGrindValue] = useState('small');
+  //radio button: cup size
+  const [cupValue, setCupValue] = useState('small');
+
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -76,20 +107,112 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Brew temperature">
+            <View style={styles.barContainer}>
+              <View style={[styles.bar, { left: barPosition }]} {...panResponder.panHandlers} />
+            </View>
+            <Text>Temperature: {percentage}%</Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          
+          <Section title="Grind size">
+            <View style={styles.radioContainer}>
+            <View style={styles.radioGroup}>
+                <View style={styles.radioButton}>
+                    <RadioButton.Android
+                        value="small"
+                        status={grindValue === 'small' ? 
+                                'checked' : 'unchecked'}
+                        onPress={() => setGrindValue('small')}
+                        color="#007BFF"
+                    />
+                    <Text style={styles.radioLabel}>
+                        Small
+                    </Text>
+                </View>
+
+                <View style={styles.radioButton}>
+                    <RadioButton.Android
+                        value="medium"
+                        status={grindValue === 'medium' ? 
+                                 'checked' : 'unchecked'}
+                        onPress={() => setGrindValue('medium')}
+                        color="#007BFF"
+                    />
+                    <Text style={styles.radioLabel}>
+                        Medium
+                    </Text>
+                </View>
+
+                <View style={styles.radioButton}>
+                    <RadioButton.Android
+                        value="big"
+                        status={grindValue === 'big' ? 
+                                'checked' : 'unchecked'}
+                        onPress={() => setGrindValue('big')}
+                        color="#007BFF"
+                    />
+                    <Text style={styles.radioLabel}>
+                        Big
+                    </Text>
+                </View>
+            </View>
+        </View>
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
+          <Section title="Cup size">
+            <View style={styles.radioContainer}>
+            <View style={styles.radioGroup}>
+                <View style={styles.radioButton}>
+                    <RadioButton.Android
+                        value="small"
+                        status={cupValue === 'small' ? 
+                                'checked' : 'unchecked'}
+                        onPress={() => setCupValue('small')}
+                        color="#007BFF"
+                    />
+                    <Text style={styles.radioLabel}>
+                        Small
+                    </Text>
+                </View>
+
+                <View style={styles.radioButton}>
+                    <RadioButton.Android
+                        value="medium"
+                        status={cupValue === 'medium' ? 
+                                 'checked' : 'unchecked'}
+                        onPress={() => setCupValue('medium')}
+                        color="#007BFF"
+                    />
+                    <Text style={styles.radioLabel}>
+                        Medium
+                    </Text>
+                </View>
+
+                <View style={styles.radioButton}>
+                    <RadioButton.Android
+                        value="big"
+                        status={cupValue === 'big' ? 
+                                'checked' : 'unchecked'}
+                        onPress={() => setCupValue('big')}
+                        color="#007BFF"
+                    />
+                    <Text style={styles.radioLabel}>
+                        Big
+                    </Text>
+                </View>
+
+            </View>
+        </View>
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="Pick up location">
+          {<DropdownComponent />}
+      </Section>
+        </View>
+        <View style={styles.buttonContainer}>
+        <Button
+          title="Purchase"
+          onPress={() => Alert.alert('Simple Button pressed')}
+          color="#007BFF"
+        />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -113,6 +236,70 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  //slidable bar
+  barContainer: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 20,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+    //padding: 20,
+  },
+  bar: {
+    position: 'absolute',
+    top: '50%',
+    width: 20,
+    height: 20,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    transform: [{ translateY: -10 }], // Center the bar vertically
+  },
+//radio button
+  radioContainer: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  radioGroup: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: 0,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    padding: 16,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  radioButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  radioLabel: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: '#333',
+},
+//button
+  buttonContainer: {
+    margin: 50,
+    //alignItems: 'center',
+    color: 'green',
+    paddingTop: 0,
+    paddingBottom: 30,
+  },
+
+
+
 });
 
 export default App;
---
+++ Dropdown.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+//import { CheckBox, Icon } from '@rneui/themed';
+/*
+type CheckboxComponentProps = {};
+
+const CheckboxComponent: React.FunctionComponent<CheckboxComponentProps> = () => {
+const [check1, setCheck1] = useState(false);
+
+return (
+
+    <CheckBox
+      center
+      title="Click Here"
+      checked={check1}
+      onPress={() => setCheck1(!check1)}
+    />
+);};
+*/
+
+  const data = [
+    { label: 'Admiralty', value: '1' },
+      { label: 'Causeway Bay', value: '2' },
+      { label: 'Kowloon Tong', value: '3' },
+      { label: 'Shatin', value: '4' },
+      { label: 'Tsuen Wan', value: '5' },
+      { label: 'Tsim Sha Tsui', value: '6' },
+      { label: 'Yau Tong', value: '7' },
+  ];
+
+  const DropdownComponent = () => {
+    const [value, setValue] = useState<string | null>(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    const renderLabel = () => {
+      if (value || isFocus) {
+        return (
+          <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+            Station
+          </Text>
+        );
+      }
+      return null;
+    };
+
+    return (
+      <View style={styles.container}>
+        {renderLabel()}
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={data}
+          search
+          maxHeight={200}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? 'Select station' : '...'}
+          searchPlaceholder="Search..."
+          value={value}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={item => {
+            setValue(item.value);
+            setIsFocus(false);
+          }}
+        />
+      </View>
+    );
+  };
+
+//export { DropdownComponent, CheckboxComponent };
+export default DropdownComponent;
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      padding: 16,
+      width: '100%',
+      height: 100,
+    },
+    dropdown: {
+      height: 50,
+      borderColor: 'gray',
+      borderWidth: 0.5,
+      borderRadius: 8,
+      paddingHorizontal: 8,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    label: {
+      position: 'absolute',
+      backgroundColor: 'white',
+      left: 22,
+      top: 8,
+      zIndex: 999,
+      paddingHorizontal: 8,
+      fontSize: 14,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  })
+  ;
