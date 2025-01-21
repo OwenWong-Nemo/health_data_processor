ab21483799d678b557e80e33339c4f71d2b1456a
OwenWong-Nemo <owenwong2003@gmail.com>
2025-01-21 10:09:23 +0800

    Testing

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@
 All options, file path to both "source file" and output file are specified here
 """
 config = {
-    "raw_xml": "/Users/owenwong/Desktop/health_data_processor/healthDate/export_demo1.xml", # Input: Raw data
-    "processed_xml": "/Users/owenwong/Desktop/health_data_processor/healthDate/filtered_demo1.xml", # Output: Filtered data 
+    "raw_xml": "/Users/owenwong/Desktop/health_data_processor/healthDate/filtered_data/filtered_demo7.xml", # Input: Raw data
+    "processed_xml": "/Users/owenwong/Desktop/health_data_processor/healthDate/raw_data/export_demo7.xml", # Output: Filtered data 
     # More options available, more detail: README
     "options": [
         "HKQuantityTypeIdentifierAppleExerciseTime",
